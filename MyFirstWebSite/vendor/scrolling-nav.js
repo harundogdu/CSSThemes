7735c155e4ac42e6647abb7446593fcc31ecbd90
Harun Doğdu <Harundogdu06@gmail.com>
2021-04-11 17:40:37 +0300

    my first web site added

---
+++ MyFirstWebSite/vendor/scrolling-nav.js
@@ -0,0 +1,26 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 50)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 70
+  });
+
+})(jQuery); // End of use strict
